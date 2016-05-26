2a59503edf5e70a9e8ebc0cb8c7400a9a488202b
Cu Le Huy <lcu@usc.edu>
2016-05-26 00:01:35 -0700

    Clean up code

--- src/data_structures/LinkedList.java
+++ src/data_structures/LinkedList.java
@@ -26,33 +26,23 @@ public class LinkedList<T> {
 			this.prev = null;
 			this.val = val;
 		}
-		
-		// Set the previous element
-		public void setPrev(Node<E> prev) {
-			this.prev = prev;
-		}
-		
-		// Set the next element
-		public void setNext(Node<E> next) {
-			this.next = next;
-		}
 	}
 	
 	// Constructor
 	public LinkedList() {
-		this.head = null;
-		this.tail = null;
-		this.size = 0;
+		head = null;
+		tail = null;
+		size = 0;
 	}
 	
 	// Return the number of elements in the list
 	public int size(){
-		return this.size;
+		return size;
 	}
 	
 	// Check whether the list is empty
 	public boolean isEmpty() {
-		return this.size == 0;
+		return size == 0;
 	}
 	
 	// Insert value val at index pos
@@ -62,88 +52,88 @@ public class LinkedList<T> {
 		
 		Node<T> newNode = new Node<>(val);
 		// Inserting into an empty list
-		if(this.isEmpty()) { 
-			this.head = newNode;
-			this.tail = head;
+		if(isEmpty()) { 
+			head = newNode;
+			tail = head;
 		}
 		// Inserting into the front of the list
 		else if(pos == 0) {
-			this.head.setPrev(newNode);
-			newNode.setNext(this.head);
-			this.head = newNode;
+			head.prev = newNode;
+			newNode.next = head;
+			head = newNode;
 		}
 		// Inserting into the end of the list
-		else if(pos == this.size()) {
-			this.tail.setNext(newNode);
-			newNode.setPrev(tail);
-			this.tail = newNode;
+		else if(pos == size()) {
+			tail.next = newNode;
+			newNode.prev = tail;
+			tail = newNode;
 		}
 		// Normal insertion
-		else if(pos > 0 && pos < this.size()){
-			newNode.setNext(this.getNodeAtIndex(pos));
-			newNode.setPrev(this.getNodeAtIndex(pos-1));
-			this.getNodeAtIndex(pos).setPrev(newNode);
-			this.getNodeAtIndex(pos-1).setNext(newNode);
+		else if(pos > 0 && pos < size()){
+			newNode.next = getNodeAtIndex(pos);
+			newNode.prev = getNodeAtIndex(pos-1);
+			getNodeAtIndex(pos).prev = newNode;
+			getNodeAtIndex(pos-1).next = newNode;
 		}
 		
 		System.out.println("'"+val+"'" + " is inserted");
-		this.size++;
+		size++;
 	}
 	
 	// Remove the node at index pos
 	public void remove(int pos) {
 		// Validate pos
-		if(this.isEmpty() || pos < 0 || pos > size-1) throw new IndexOutOfBoundsException();
+		if(isEmpty() || pos < 0 || pos > size-1) throw new IndexOutOfBoundsException();
 		
-		System.out.println("'"+this.get(pos)+"'" + " is removed");
+		System.out.println("'"+get(pos)+"'" + " is removed");
 		
 		// Remove only element
-		if (this.size() == 1) {
-			this.head = null;
-			this.tail = null;
+		if (size() == 1) {
+			head = null;
+			tail = null;
 		}
 		// Remove head
 		else if(pos == 0) {
-			this.head = this.head.next;
-			this.head.setPrev(null);
+			head = head.next;
+			head.prev = null;
 		}
 		// Remove tail
-		else if(pos == this.size()-1) {
-			this.tail = this.tail.prev;
-			this.tail.setNext(null);
+		else if(pos == size()-1) {
+			tail = tail.prev;
+			tail.next = null;
 		}
 		// Normal removal
-		else if(pos > 0 && pos < this.size()) {
-			Node<T> tempPrev = this.getNodeAtIndex(pos-1);
-			Node<T> tempNext = this.getNodeAtIndex(pos+1);
-			tempPrev.setNext(tempNext);
-			tempNext.setPrev(tempPrev);
+		else if(pos > 0 && pos < size()) {
+			Node<T> tempPrev = getNodeAtIndex(pos-1);
+			Node<T> tempNext = getNodeAtIndex(pos+1);
+			tempPrev.next = tempNext;
+			tempNext.prev = tempPrev;
 		}
 		
-		this.size--;
+		size--;
 	}
 	
 	// Overwrites the old value at index pos with value val
 	public void set(int pos, T val) {
 		// Validate pos
-		if(this.isEmpty() || pos < 0 || pos > size-1) throw new IndexOutOfBoundsException();
+		if(isEmpty() || pos < 0 || pos > size-1) throw new IndexOutOfBoundsException();
 		
-		this.getNodeAtIndex(pos).val = val;
+		getNodeAtIndex(pos).val = val;
 		System.out.println("The "+pos+"th element is set to "+val);
 	}
 	
 	// Return the value at index pos
 	public T get(int pos) {
 		// Validate pos
-		if(this.isEmpty() || pos < 0 || pos > size-1) throw new IndexOutOfBoundsException();
+		if(isEmpty() || pos < 0 || pos > size-1) throw new IndexOutOfBoundsException();
 		
-		return this.getNodeAtIndex(pos).val;
+		return getNodeAtIndex(pos).val;
 	}
 	
 	// Return the node at specified index pos
 	public Node<T> getNodeAtIndex(int pos) {
-		Node<T> temp = this.head;
-		if(pos >= 0 && pos < this.size()) {
+		Node<T> temp = head;
+		if(pos >= 0 && pos < size()) {
 			for(int i=0; i<pos; i++) {
 				temp = temp.next;
 			}
@@ -158,7 +148,7 @@ public class LinkedList<T> {
 		Node<T> curr = this.head;
 		
 		// The list is empty
-		if(this.isEmpty()) System.out.print("The list is empty");
+		if(isEmpty()) System.out.print("The list is empty");
 
 		// The list is not empty
 		else {
