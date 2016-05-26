949b750aa0bdc77fe283f19c89d1c69f288aa15c
Cu Le Huy <lcu@usc.edu>
2016-05-25 22:40:28 -0700

    Clean up code

--- src/data_structures/LinkedList.java
+++ src/data_structures/LinkedList.java
@@ -1,16 +1,43 @@
-package linkedlist;
+package data_structures;
 
 /**
  * 
  * @author Cu Le Huy
  * @param <T>
- * last updated: 05/24/2016
+ * last updated: 05/25/2016
  */
 
 public class LinkedList<T> {
 	private Node<T> head;
 	private Node<T> tail;
 	private int size;
+	
+	
+	/**
+	 * Node class
+	 */
+	private class Node<E> {
+		E val;
+		Node<E> next;
+		Node<E> prev;
+		
+		// Node constructor
+		public Node(E val){
+			this.next = null;
+			this.prev = null;
+			this.val = val;
+		}
+		
+		// Set the previous element
+		public void setPrev(Node<E> prev) {
+			this.prev = prev;
+		}
+		
+		// Set the next element
+		public void setNext(Node<E> next) {
+			this.next = next;
+		}
+	}
 	
 	// Constructor
 	public LinkedList() {
@@ -31,8 +58,12 @@ public class LinkedList<T> {
 	
 	// Insert value val at index pos
 	public void insert(int pos, T val) {
-		Node<T> newNode = new Node<>(val);
+		// Validate pos
+		if(pos < 0 || pos > size) {
+			return;
+		}
 		
+		Node<T> newNode = new Node<>(val);
 		// Inserting into an empty list
 		if(this.isEmpty()) { 
 			this.head = newNode;
@@ -58,6 +89,7 @@ public class LinkedList<T> {
 			this.getNodeAtIndex(pos-1).setNext(newNode);
 		}
 		
+		System.out.println("'"+val+"'" + " is inserted");
 		this.size++;
 	}
 	
@@ -67,8 +99,11 @@ public class LinkedList<T> {
 		if(this.isEmpty() || pos < 0 || pos > size-1) {
 			return;
 		}
+		
+		System.out.println("'"+this.get(pos)+"'" + " is removed");
+		
 		// Remove only element
-		else if (this.size() == 1) {
+		if (this.size() == 1) {
 			this.head = null;
 			this.tail = null;
 		}
@@ -95,11 +130,20 @@ public class LinkedList<T> {
 	
 	// Overwrites the old value at index pos with value val
 	public void set(int pos, T val) {
+		// Validate pos
+		if(this.isEmpty() || pos < 0 || pos > size-1) {
+			return;
+		}
 		this.getNodeAtIndex(pos).val = val;
+		System.out.println("The "+pos+"th element is set to "+val);
 	}
 	
 	// Return the value at index pos
 	public T get(int pos) {
+		// Validate pos
+		if(this.isEmpty() || pos < 0 || pos > size-1) {
+			return null;
+		}
 		return this.getNodeAtIndex(pos).val;
 	}
 	
@@ -134,31 +178,5 @@ public class LinkedList<T> {
 			}
 		}
 		System.out.println();
-	}
-	
-	/**
-	 * Node class
-	 */
-	public class Node<E> {
-		public E val;
-		Node<E> next;
-		Node<E> prev;
-		
-		// Constructor
-		public Node(E val){
-			this.next = null;
-			this.prev = null;
-			this.val = val;
-		}
-		
-		// Set the previous element
-		public void setPrev(Node<E> prev) {
-			this.prev = prev;
-		}
-		
-		// Set the next element
-		public void setNext(Node<E> next) {
-			this.next = next;
-		}
 	}
 }
